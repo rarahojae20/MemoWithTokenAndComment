0b764df8e13e4d902b0f4e384750c0a7d34f1906
rarahojae20 <91858512+rarahojae20@users.noreply.github.com>
2023-11-22 21:49:17 +0900

    Update comment.router.ts

--- src/api/comment/comment.router.ts
+++ src/api/comment/comment.router.ts
@@ -5,11 +5,6 @@ import CommentController from './comment.controller';
 export const commentRouter = Router();
 
 commentRouter.post('/', authenticateUser, new CommentController().create);
-// commentRouter.get('/', authenticateUser, new CommentController().findAll);
-// commentRouter.get('/:commentId', authenticateUser, new CommentController().findOne);
-// commentRouter.put('/:commentId', authenticateUser, new CommentController().update);
 commentRouter.delete('/:id', authenticateUser, new CommentController().delete);
-
-
 
 export default commentRouter;
