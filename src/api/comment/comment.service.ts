ffcef650d736711b8fcac48b91af62b8d4caebd2
rarahojae20 <91858512+rarahojae20@users.noreply.github.com>
2023-11-22 21:49:42 +0900

    Update comment.service.ts

--- src/api/comment/comment.service.ts
+++ src/api/comment/comment.service.ts
@@ -3,12 +3,12 @@ import CommentRepository from './comment.repository'; // CommentRepository impor
 
 export default class CommentService {
   public create = async (commentData: IComment): Promise<IComment> => {
-    const result = await new CommentRepository().create(commentData); // CommentRepository의 create 메서드 사용
+    const result = await new CommentRepository().create(commentData); 
     return result;
   };
 
   public delete = async (id: number): Promise<IComment | null> => {
-    const result = await new CommentRepository().delete(id); // CommentRepository의 delete 메서드 사용
+    const result = await new CommentRepository().delete(id);
     return result;
   };
 }
